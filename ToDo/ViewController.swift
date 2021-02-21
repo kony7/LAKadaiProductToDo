e772af9d381a2ae9f624b6b228d36d7e0b65064c
Kony <fzf55747@kwansei.ac.jp>
2021-02-22 01:24:48 +0900

    最初の画面に、MemoPadのTableViewをそのまま持ってきたが、これを修正する余力なし

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    var taskArray: Array<Any>!
+    var titleDaysArray: Array<Any>!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        table.dataSource = self
+        table.delegate = self
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return  taskArray.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+           
+           cell?.textLabel?.text = titleDaysArray[indexPath.row]
+           
+           cell?.imageView?.image = UIImage(named: titleDaysArray[indexPath.row])
+           
+           return cell!
+           
+       }
 
 }
 
